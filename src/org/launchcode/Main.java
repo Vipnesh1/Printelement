cbcc47e1b9fd5023a7711e139fa3776c30847a1e
Vipnesh Chauhan <it19130@glbitm.ac.in>
2022-01-13 18:14:55 +0530

    Initial commit

---
+++ src/org/launchcode/Main.java
@@ -0,0 +1,14 @@
+package org.launchcode;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int []arr={1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<arr.length;i++)
+        {
+           System.out.println(arr[i]+"");
+        }
+    }
+}
